43a02b3cc6e33d57fd1058e5e7eae7ca3c721917
Fabricio Caneva <hardlymersk@gmail.com>
2020-11-15 10:02:18 -0300

    add overloads with a param map

--- src/main/java/me/caneva20/messagedispatcher/dispachers/IMessageDispatcher.java
+++ src/main/java/me/caneva20/messagedispatcher/dispachers/IMessageDispatcher.java
@@ -2,16 +2,28 @@ package me.caneva20.messagedispatcher.dispachers;
 
 import org.bukkit.command.CommandSender;
 
+import java.util.Map;
+
 public interface IMessageDispatcher {
     void raw(CommandSender to, String message);
 
     void info(CommandSender to, String message);
 
+    void info(CommandSender to, String message, Map<String, String> params);
+
     void warn(CommandSender to, String message);
+
+    void warn(CommandSender to, String message, Map<String, String> params);
 
     void success(CommandSender to, String message);
 
+    void success(CommandSender to, String message, Map<String, String> params);
+
     void error(CommandSender to, String message);
 
+    void error(CommandSender to, String message, Map<String, String> params);
+
     void debug(CommandSender to, String message);
+
+    void debug(CommandSender to, String message, Map<String, String> params);
 }
--- src/main/java/me/caneva20/messagedispatcher/dispachers/MessageDispatcher.java
+++ src/main/java/me/caneva20/messagedispatcher/dispachers/MessageDispatcher.java
@@ -2,9 +2,11 @@ package me.caneva20.messagedispatcher.dispachers;
 
 import me.caneva20.messagedispatcher.MessageLevel;
 import me.caneva20.messagedispatcher.parsing.IMessageParser;
-import me.caneva20.messagedispatcher.utils.Params;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+
+import java.util.Collections;
+import java.util.Map;
 
 public abstract class MessageDispatcher implements IMessageDispatcher {
     protected final IMessageParser parser;
@@ -13,14 +15,14 @@ public abstract class MessageDispatcher implements IMessageDispatcher {
         this.parser = parser;
     }
 
-    protected String format(String rawMessage, MessageLevel level, Params params) {
-        String message = parser.parse(rawMessage, level, params.build());
+    protected String format(String rawMessage, MessageLevel level, Map<String, String> params) {
+        String message = parser.parse(rawMessage, level, params);
 
         return ChatColor.translateAlternateColorCodes('&', message);
     }
 
     protected String format(String rawMessage, MessageLevel level) {
-        return format(rawMessage, level, Params.EMPTY_PARAMS);
+        return format(rawMessage, level, Collections.emptyMap());
     }
 
     @Override
