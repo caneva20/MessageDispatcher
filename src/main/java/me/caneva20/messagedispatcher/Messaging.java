f453529b542375a9c5724c60b8d750b0760a3470
Fabricio Caneva <hardlymersk@gmail.com>
2021-01-17 19:21:22 -0300

    reformat

--- src/main/java/me/caneva20/messagedispatcher/Messaging.java
+++ src/main/java/me/caneva20/messagedispatcher/Messaging.java
@@ -12,66 +12,70 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 public final class Messaging {
-    private static final Map<Class<? extends IConfiguration>, IConfiguration> configMap = new HashMap<>();
 
-    /**
-     * Returns an instance of a configuration file that can either be used
-     * to fetch a configuration for some feature or to set parameters that
-     * a feature might use
-     *
-     * @param klass The class to configure
-     * @return An instance of {@code kclass}
-     */
-    @SuppressWarnings("unchecked")
-    @NotNull
-    public static <T extends IConfiguration> T get(Class<T> klass) {
-        var config = configMap.getOrDefault(klass, null);
+  private static final Map<Class<? extends IConfiguration>, IConfiguration> configMap = new HashMap<>();
 
-        if (config == null) {
-            try {
-                config = klass.newInstance();
+  /**
+   * Returns an instance of a configuration file that can either be used to fetch a configuration
+   * for some feature or to set parameters that a feature might use
+   *
+   * @param klass The class to configure
+   * @return An instance of {@code kclass}
+   */
+  @SuppressWarnings("unchecked")
+  @NotNull
+  public static <T extends IConfiguration> T get(Class<T> klass) {
+    var config = configMap.getOrDefault(klass, null);
 
-                configMap.put(klass, config);
-            } catch (Exception e) {
-                System.out.println("Could not configure type " + klass.getName() + ". Does it contain a parameterless constructor?");
+    if (config == null) {
+      try {
+        config = klass.newInstance();
 
-                throw new RuntimeException(e);
-            }
-        }
+        configMap.put(klass, config);
+      } catch (Exception e) {
+        System.out.println("Could not configure type " + klass.getName()
+            + ". Does it contain a parameterless constructor?");
 
-        return (T) config;
+        throw new RuntimeException(e);
+      }
     }
 
-    public static <T extends IConfiguration> void configure(Class<T> klass, Consumer<T> fn) {
-        fn.accept(get(klass));
+    return (T) config;
+  }
+
+  public static <T extends IConfiguration> void configure(Class<T> klass, Consumer<T> fn) {
+    fn.accept(get(klass));
+  }
+
+  public static <T extends IConfiguration> void configure(Class<T> klass) {
+    get(klass);
+  }
+
+  public static IMessageDispatcher createDispatcher(String tag, boolean useTag) {
+    var defaultConfiguration = get(DefaultConfiguration.class);
+
+    if (useTag) {
+      var tokenConfiguration = get(TokenConfiguration.class);
+      return new PlayerMessageDispatcher(defaultConfiguration.getMessageParser(), tag,
+          tokenConfiguration);
     }
 
-    public static <T extends IConfiguration> void configure(Class<T> klass) {
-        get(klass);
-    }
+    return new UntaggedPlayerMessageDispatcher(defaultConfiguration.getMessageParser());
+  }
 
-    public static IMessageDispatcher createDispatcher(String tag, boolean useTag) {
-        var defaultConfiguration = get(DefaultConfiguration.class);
+  public static IMessageDispatcher createDispatcher(JavaPlugin plugin, boolean useTag) {
+    return createDispatcher(plugin.getName(), useTag);
+  }
 
-        if (useTag) {
-            var tokenConfiguration = get(TokenConfiguration.class);
-            return new PlayerMessageDispatcher(defaultConfiguration.getMessageParser(), tag, tokenConfiguration);
-        }
+  public static IConsoleMessageDispatcher createConsoleDispatcher(JavaPlugin plugin, String tag,
+      boolean useTag) {
+    var dispatcher = createDispatcher(tag, useTag);
 
-        return new UntaggedPlayerMessageDispatcher(defaultConfiguration.getMessageParser());
-    }
+    return new ConsoleMessageDispatcher(plugin, dispatcher);
+  }
 
-    public static IMessageDispatcher createDispatcher(JavaPlugin plugin, boolean useTag) {
-        return createDispatcher(plugin.getName(), useTag);
-    }
-
-    public static IConsoleMessageDispatcher createConsoleDispatcher(JavaPlugin plugin, String tag, boolean useTag) {
-        var dispatcher = createDispatcher(tag, useTag);
-
-        return new ConsoleMessageDispatcher(plugin, dispatcher);
-    }
-
-    public static IConsoleMessageDispatcher createConsoleDispatcher(JavaPlugin plugin, boolean useTag) {
-        return createConsoleDispatcher(plugin, plugin.getName(), useTag);
-    }
+  public static IConsoleMessageDispatcher createConsoleDispatcher(JavaPlugin plugin,
+      boolean useTag) {
+    return createConsoleDispatcher(plugin, plugin.getName(), useTag);
+  }
 }
--- src/main/java/me/caneva20/messagedispatcher/dispachers/ConsoleMessageDispatcher.java
+++ src/main/java/me/caneva20/messagedispatcher/dispachers/ConsoleMessageDispatcher.java
@@ -6,66 +6,67 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Map;
 
 public class ConsoleMessageDispatcher implements IConsoleMessageDispatcher {
-    private final IMessageDispatcher dispatcher;
-    private final ConsoleCommandSender console;
 
-    public ConsoleMessageDispatcher(JavaPlugin plugin, IMessageDispatcher dispatcher) {
-        this.dispatcher = dispatcher;
-        console = plugin.getServer().getConsoleSender();
-    }
+  private final IMessageDispatcher dispatcher;
+  private final ConsoleCommandSender console;
 
-    @Override
-    public void raw(String message) {
-        dispatcher.raw(console, message);
-    }
+  public ConsoleMessageDispatcher(JavaPlugin plugin, IMessageDispatcher dispatcher) {
+    this.dispatcher = dispatcher;
+    console = plugin.getServer().getConsoleSender();
+  }
 
-    @Override
-    public void info(String message, Map<String, String> params) {
-        dispatcher.info(console, message, params);
-    }
+  @Override
+  public void raw(String message) {
+    dispatcher.raw(console, message);
+  }
 
-    @Override
-    public void info(String message) {
-        dispatcher.info(console, message);
-    }
+  @Override
+  public void info(String message, Map<String, String> params) {
+    dispatcher.info(console, message, params);
+  }
 
-    @Override
-    public void warn(String message, Map<String, String> params) {
-        dispatcher.warn(console, message, params);
-    }
+  @Override
+  public void info(String message) {
+    dispatcher.info(console, message);
+  }
 
-    @Override
-    public void warn(String message) {
-        dispatcher.warn(console, message);
-    }
+  @Override
+  public void warn(String message, Map<String, String> params) {
+    dispatcher.warn(console, message, params);
+  }
 
-    @Override
-    public void success(String message, Map<String, String> params) {
-        dispatcher.success(console, message, params);
-    }
+  @Override
+  public void warn(String message) {
+    dispatcher.warn(console, message);
+  }
 
-    @Override
-    public void success(String message) {
-        dispatcher.success(console, message);
-    }
+  @Override
+  public void success(String message, Map<String, String> params) {
+    dispatcher.success(console, message, params);
+  }
 
-    @Override
-    public void error(String message, Map<String, String> params) {
-        dispatcher.error(console, message, params);
-    }
+  @Override
+  public void success(String message) {
+    dispatcher.success(console, message);
+  }
 
-    @Override
-    public void error(String message) {
-        dispatcher.error(console, message);
-    }
+  @Override
+  public void error(String message, Map<String, String> params) {
+    dispatcher.error(console, message, params);
+  }
 
-    @Override
-    public void debug(String message, Map<String, String> params) {
-        dispatcher.debug(console, message, params);
-    }
+  @Override
+  public void error(String message) {
+    dispatcher.error(console, message);
+  }
 
-    @Override
-    public void debug(String message) {
-        dispatcher.debug(console, message);
-    }
+  @Override
+  public void debug(String message, Map<String, String> params) {
+    dispatcher.debug(console, message, params);
+  }
+
+  @Override
+  public void debug(String message) {
+    dispatcher.debug(console, message);
+  }
 }
